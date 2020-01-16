b262784136ecc2f5f846b465bf73818784265570
Jesper Asplund <jesper.asplund95@gmail.com>
2020-01-16 15:48:53 +0100

    if admin allow to start editing article

--- src/views/ArticlePage.vue
+++ src/views/ArticlePage.vue
@@ -1,27 +1,39 @@
 <template>
-  <Loader v-if="this.state.loading"/>
-  <b-container v-else-if="this.state.article._id" class="wrapper article-page">
-    {{this.state.article.headline}}
-    <br />
-    {{this.state.article.image}}
-    <br />
-    {{this.state.article.body}}
+  <Loader v-if="this.state.loading" />
+  <b-container v-else-if="this.state.article._id" class="wrapper">
+    <div v-if="this.state.loggedIn" class="text-right">
+      <button @click="toggleEdit" class="btn" :class="this.state.edit ? 'btn-danger' : 'btn-info'">
+        <span v-if="this.state.edit">Avbryt</span>
+        <span v-else>Redigera</span>
+      </button>
+    </div>
+    <Article v-if="!this.state.edit" :articleData="this.state.article" />
+    <EditArticle
+      v-else-if="this.state.edit"
+      :edit="this.state.edit"
+      :toggleEdit="toggleEdit"
+      :article="this.state.article"
+    />
+    <!-- <EditArticle v-if="this.state.edit" :articleData="this.state.article" /> -->
   </b-container>
   <MissingPage v-else />
 </template>
 <script>
 import axios from "axios";
-import MissingPage from '../views/MissingPage'
-import Loader from '../components/Loader'
+import MissingPage from "../views/MissingPage";
+import Loader from "../components/Loader";
+import Article from "../components/Article";
+import EditArticle from "../components/EditArticle";
 export default {
   name: "articlepage",
-  components: {MissingPage, Loader},
+  components: { MissingPage, Loader, Article, EditArticle },
   data() {
     return {
       state: {
         loading: true,
         article: {},
-        loggedIn: false
+        loggedIn: false,
+        edit: false
       }
     };
   },
@@ -33,17 +45,27 @@ export default {
         url: `/api/articles/${this.$route.params.link}`
       });
       this.state.article = article.data;
-      this.state.loading = false
+      this.state.loading = false;
     } else {
       const article = await axios({
         method: "GET",
         url: `/api/articles/published/${this.$route.params.link}`
       });
       this.state.article = article.data;
-      this.state.loading = false
+      this.state.loading = false;
     }
   },
   methods: {
+    toggleEdit(event, article) {
+      if (article) {
+        this.state.edit = false;
+        this.state.article.headline = article.headline;
+        this.state.article.image = article.image;
+        this.state.article.body = article.body;
+      } else {
+        this.state.edit = !this.state.edit;
+      }
+    },
     async checkIfLoggedIn() {
       let response = await axios({
         method: "GET",
@@ -57,7 +79,4 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.article-page {
-  color: red;
-}
 </style>
