6800b540754b6b760aa05dd928cfaaa6d30d8ab3
Jesper Asplund <jesper.asplund95@gmail.com>
2020-01-16 15:48:30 +0100

    ckeditor

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import "./registerServiceWorker"
 import router from "./router"
 import store from "./store"
 
-//Import of bootstrap
+// Import of bootstrap
 import './scss/bootstrap.scss'
 import BootstrapVue from 'bootstrap-vue'
+
+// WYSIWYG Editor
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
 
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(CKEditor);
 new Vue({
   router,
   store,
